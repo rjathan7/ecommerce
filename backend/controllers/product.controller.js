9b3e90bccd49c8812d859dae9ab001f0a38f8cf3
rjathan7 <rjathan1602@gmail.com>
2025-07-25 15:41:02 -0700

    implemented add and delete functionality for products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import { redis } from "../lib/redis.js";
+import cloudinary from "../lib/cloudinary.js"
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -33,6 +34,82 @@ export const getFeaturedProducts = async (req, res) => {
         res.json(featuredProducts);
     } catch (error) {
         console.log("Error in getFeaturedProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export const createProduct = async (req, res) => {
+    try {
+        const {name, descrption, price, image, category} = req.body;
+
+        let cloudinaryResponse = null;
+
+        if (image) {
+            await cloudinary.uploader.upload(image, {folder:"products"})
+        }
+
+        const product = await Product.create({
+            name,
+            decription,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url: "",
+            category
+        })
+
+        res.status(201).json(product)
+    } catch (error) {
+        console.log("Error in createProduct controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        if (product.image) {
+            const publicId = product.image.split("/").pop().split(".")[0]; // this will get the id of the image
+            try {
+                await cloudinary.uploader.destroy(`products/${publicId}`);
+                console.log("deleted image from cloudinary");
+            } catch (error) {
+                console.log("error deleted image from cloudinary", error)
+            }
+        }
+
+        await Product.findByIdAndDelete(req.params.id);
+
+        res.json({ message: "Product deleted successfully" })
+    } catch (error) {
+        console.log("Error in createProduct controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export const getRecommendedProducts = async (req, res) => {
+    try {
+        const products = await Product.aggregate([
+            {
+                $sample: {size:3} // randomly select 3 products
+            },
+            {
+                $project:{
+                    _id:1,
+                    name:1,
+                    description:1,
+                    image:1,
+                    price:1
+                } // 1 -> include this field in the output
+            }
+        ])
+
+        res.json(products);
+    } catch (error) {
+        console.log("Error in getRecommendedProducts controller", error.message);
         res.status(500).json({ message: "Server error", error: error.message });
     }
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct, getRecommendedProducts } from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -8,5 +8,8 @@ const router = express.Router();
 // user has to be authenticated and an admin before they can get all products
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/recommendations", getRecommendedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
