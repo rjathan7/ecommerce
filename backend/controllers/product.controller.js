3cdfc9c99c320cecc1285cee1c35c53d55d034e2
rjathan7 <rjathan1602@gmail.com>
2025-07-25 13:45:15 -0700

    added middlware for protecting route and admin checking

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,23 @@
-export const getAllProducts = () => {
-    
+import { redis } from "../lib/redis.js";
+import Product from "../models/product.model.js";
+
+export const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({}); // find all products
+        res.json({ products });
+    } catch (error) {
+        console.log("Error in getAllProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let featuredProducts = await redis.get("featured_products");
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));
+        }
+    } catch (error) {
+
+    }
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
+// protectRoute and adminRoute are middleware
+// user has to be authenticated and an admin before they can get all products
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
