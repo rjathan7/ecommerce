02107e342ab3f355df135f2bf9136fd90271f5c7
rjathan7 <rjathan1602@gmail.com>
2025-08-05 23:03:16 -0700

    implemented delete product functionality

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -6,7 +6,7 @@ import AnalyticsTab from "../components/AnalyticsTab";
 import CreateProductForm from "../components/CreateProductForm";
 import ProductsList from "../components/ProductsList";
 
-// import { useProductStore } from "../stores/useProductStore";
+import { useProductStore } from "../stores/useProductStore";
 
 const tabs = [
 	{ id: "create", label: "Create Product", icon: PlusCircle },
@@ -16,11 +16,11 @@ const tabs = [
 
 const AdminPage = () => {
 	const [activeTab, setActiveTab] = useState("create");
-	// const { fetchAllProducts } = useProductStore();
+	const { fetchAllProducts } = useProductStore();
 
-	// useEffect(() => {
-	// 	fetchAllProducts();
-	// }, [fetchAllProducts]);
+	useEffect(() => {
+		fetchAllProducts();
+	}, [fetchAllProducts]);
 
 	return (
 		<div className='min-h-screen relative overflow-hidden'>
